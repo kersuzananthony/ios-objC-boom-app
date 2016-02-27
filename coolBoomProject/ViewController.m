f4f4f5b54bb3c5fc23b550d5ed46e294d386dc6f
Kersuzan <kersuzan.anthony@gmail.com>
2016-02-27 15:38:37 +0800

    Initial commit

--- coolBoomProject/ViewController.m
+++ coolBoomProject/ViewController.m
@@ -9,6 +9,9 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UIImageView *backgroundImage;
+@property (weak, nonatomic) IBOutlet UIImageView *boomAppImage;
+@property (weak, nonatomic) IBOutlet UIButton *makeMeCoolButton;
 
 @end
 
@@ -22,6 +25,12 @@
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)makeMeCool:(UIButton *)sender {
+    self.backgroundImage.hidden = NO;
+    self.boomAppImage.hidden = NO;
+    self.makeMeCoolButton.hidden = YES;
 }
 
 @end
